797043da70691e66074a7af554de8657620be35b
j.kim <jayfulkr@gmail.com>
2024-07-30 18:00:24 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import TicketItem from './components/molecules/TicketItem.vue';
+</script>
+
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <TicketItem/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/molecules/TicketItem.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import gsap from 'gsap';
+
+    
+    const handleCard = (e:MouseEvent, ratio: number) => {
+        const x = gsap.utils.mapRange(0, 700, -1, 1);
+        const y = gsap.utils.mapRange(0, 250, -1, 1);
+        const target = e.target as HTMLElement; 
+        target.style.transform = `
+            perspective(400px)
+            rotateX(${y(e.offsetY) * ratio}deg)
+            rotateY(${x(e.offsetX) * ratio * -1}deg)
+        `
+    }
+    const handleLeave = (e:MouseEvent) => {
+        const target = e.target as HTMLDivElement
+        target.style.transform = `
+            rotateX(0deg)
+            rotateY(0deg)
+        `
+    };
+
+</script>
+<template>
+    <div ref="ani" @mousemove="(e) => handleCard(e, 5)" @mouseleave="e => handleLeave(e)"
+        class="flex w-[700px] h-[250px] drop-shadow-xl">
+
+        <div class="flex flex-col w-full h-full rounded-xl overflow-hidden pointer-events-none">
+
+            <div class="flex items-center w-full h-[64px] px-4 bg-blue-500 ">
+                <img src="../../assets/logo.png" class="w-[32px] h-[32px]" alt="gxpのロゴ">
+                <h1 class="text-white font-bold ml-2">GXP AIRLINE</h1>
+            </div>
+
+            <div class="flex gap-4 h-full w-full p-4 bg-white overflow-hidden">
+                <img src="../../assets/poke.png" class="w-32 h-full object-cover" alt="プロフィール写真">
+                <div class="relative flex flex-col justify-around w-full h-full bg-[url('/src/assets/map.png')] bg-no-repeat bg-[-420px_-120px] bg-[length:200%] ">
+                    <svg class="absolute top-0 left-0" width="100%" height="100%">
+                        <path d="M140,148 A 80,80 0 0 1 208,51" stroke="red" stroke-width="2" fill="none" stroke-dasharray="5,5" stroke-linecap="round"/>
+                    </svg>
+                    <div class="flex justify-around">
+                        <div class="text-center">
+                            <h3 class=" text-4xl font-bold">CGK</h3>
+                            <p><small class="">jakarta</small></p>
+                            <p class="font-bold">2024/00/00</p>
+                        </div>
+                        <div class="text-center">
+                            <h3 class=" text-4xl font-bold">CGK</h3>
+                            <p><small class="">jakarta</small></p>
+                            <p class="font-bold">2024/00/00</p>
+                        </div>
+                    </div>
+                    <div class="border border-orange-400 h-16">
+                        contents
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="relative flex flex-col justify-between items-center w-[200px] h-full bg-white overflow-hidden rounded-xl pointer-events-none">
+            <div class="relative flex items-center h-[64px] w-full px-4 bg-blue-400">
+                <div class="absolute left-0 top-0 h-full w-0  border-l border-dotted border-white"></div>
+                <div class="border border-orange-400 w-full">contents</div>
+            </div>
+            <div class="relative flex flex-col justify-between items-center gap-2 px-4 py-2 h-full">
+                <div class="absolute left-0 top-0 h-full w-0  border-l border-dotted border-gray-400">
+                </div>
+                <div class="border border-orange-400 w-full h-full">
+                    contents
+                </div>
+                <img src="../../assets/barcode.png" class="" alt="バーコード">
+            </div>
+            <div class="absolute top-1/2 left-1/2 w-[150%] translate-x-[-50%] translate-y-[-50%]">
+                <img src="../../assets/gxplogo.png" class="w-full h-full" alt="">
+            </div>
+        </div>
+
+    </div>
+</template>
