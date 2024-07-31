cc0a0a61328b6fce817d474ac243b70e2daa9672
j.kim <j.kim@gxp.co.jp>
2024-07-31 11:20:54 +0900

    grid

--- src/components/molecules/TicketItem.vue
+++ src/components/molecules/TicketItem.vue
@@ -23,20 +23,19 @@ import gsap from 'gsap';
 </script>
 <template>
     <div ref="ani" @mousemove="(e) => handleCard(e, 5)" @mouseleave="e => handleLeave(e)"
-        class="flex w-[700px] h-[250px] drop-shadow-xl">
+        class="grid grid-cols-[500px_200px] w-[700px] h-[250px] drop-shadow-xl">
 
-        <div class="flex flex-col w-full h-full rounded-xl overflow-hidden pointer-events-none">
-
-            <div class="flex items-center w-full h-[64px] px-4 bg-gradient-to-r from-blue-500 to-blue-400 from-50%">
+        <div class="grid grid-rows-[48px_1fr] rounded-xl overflow-hidden pointer-events-none">
+            <div class="flex items-center px-4 bg-gradient-to-r from-blue-500 to-blue-400 from-40%">
                 <img src="../../assets/logo.png" class="w-[32px] h-[32px]" alt="gxpのロゴ">
                 <h1 class="text-white font-bold ml-2">GXP AIRLINE</h1>
             </div>
 
-            <div class="flex gap-4 h-full w-full p-4 bg-white overflow-hidden">
+            <div class="flex gap-4 p-4 bg-white overflow-hidden">
                 <img src="../../assets/poke.png" class="w-32 h-full object-cover" alt="プロフィール写真">
-                <div class="relative flex flex-col justify-around w-full h-full bg-[url('/src/assets/map.png')] bg-no-repeat bg-[-420px_-120px] bg-[length:200%] ">
+                <div class="relative flex flex-col justify-around w-full h-full bg-[url('/src/assets/map.png')] bg-no-repeat bg-[-500px_-150px] bg-[length:250%] fade-out-border">
                     <svg class="absolute top-0 left-0" width="100%" height="100%">
-                        <path d="M140,148 A 80,80 0 0 1 208,51" stroke="red" stroke-width="2" fill="none" stroke-dasharray="5,5" stroke-linecap="round"/>
+                        <path d="M119,145 A 80,80 0 0 1 192,38" stroke="red" stroke-width="2" fill="none" stroke-dasharray="5,5" stroke-linecap="round"/>
                     </svg>
                     <div class="flex justify-around">
                         <div class="text-center">
@@ -57,12 +56,12 @@ import gsap from 'gsap';
             </div>
         </div>
 
-        <div class="relative flex flex-col justify-between items-center w-[200px] h-full bg-white overflow-hidden rounded-xl pointer-events-none">
-            <div class="relative flex items-center h-[64px] w-full px-4 bg-blue-400">
-                <div class="absolute left-0 top-0 h-full w-0  border-l border-dotted border-white"></div>
+        <div class="relative grid grid-rows-[48px_1fr] bg-white overflow-hidden rounded-xl pointer-events-none">
+            <div class="relative flex items-center px-4 bg-blue-400">
+                <div class="absolute left-0 top-0 h-[48px] w-0  border-l border-dotted border-white"></div>
                 <div class="border border-orange-400 w-full">contents</div>
             </div>
-            <div class="relative flex flex-col justify-between items-center gap-2 px-4 py-2 h-full">
+            <div class="relative flex flex-col justify-between items-center gap-2 px-4 py-2">
                 <div class="absolute left-0 top-0 h-full w-0  border-l border-dotted border-gray-400">
                 </div>
                 <div class="border border-orange-400 w-full h-full">
